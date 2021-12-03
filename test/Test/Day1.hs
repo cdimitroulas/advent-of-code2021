002bb2da19dc0256cda2a43273729139174c8418
cdimitroulas <c.dimitroulas@gmail.com>
2021-12-03 09:27:27 +0000

    Extract filepath into variable

--- test/Test/Day1.hs
+++ test/Test/Day1.hs
@@ -3,9 +3,12 @@ module Test.Day1 where
 import           Day1
 import           Test.Syd
 
+exampleDataFilePath :: String
+exampleDataFilePath = "data/day1-example.txt"
+
 day1Spec :: Spec
 day1Spec = do
-  depths <- runIO $ map read . lines <$> readFile "data/day1-example.txt"
+  depths <- runIO $ map read . lines <$> readFile exampleDataFilePath
 
   describe "getDepthIncreases" $ do
     it "returns the correct number of depth increases for the example data" $ do
